58b1659180d3693aa5b5aa46a34c5d668fd19515
wandl <hiwepy@163.com>
2024-08-09 19:32:18 +0800

    feat: init 示例

--- langchain4j-ollama-rag-weaviate/src/main/java/com/github/teachingai/ollama/Langchain4jOllamaRagApplication.java
+++ langchain4j-ollama-rag-weaviate/src/main/java/com/github/teachingai/ollama/Langchain4jOllamaRagApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringAiOllamaApplication {
+public class Langchain4jOllamaRagApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringAiOllamaApplication.class, args);
+        SpringApplication.run(Langchain4jOllamaRagApplication.class, args);
     }
 
 }
--- langchain4j-ollama-rag-weaviate/src/test/java/com/github/teachingai/ollama/Langchain4jOllamaRagApplicationTests.java
+++ langchain4j-ollama-rag-weaviate/src/test/java/com/github/teachingai/ollama/Langchain4jOllamaRagApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringAiOllamaApplicationTests {
+class Langchain4jOllamaRagApplicationTests {
 
     @Test
     void contextLoads() {
